74df83a5a574cf3b074f55c7b1025cc211831721
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-09-09 01:09:33 -0400

    7out21-update1

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,5 +1,18 @@
-function find() { // EXERCISE A
-  /*
+const db = require("../../data/db-config");
+module.exports = {
+  async find() {
+    const results = await db("schemes as sc")
+      .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+      .select("sc.*")
+      .count("st.step_id as number_of_steps")
+      .groupBy("sc.scheme_id");
+    return results;
+  },
+};
+
+// function find() {
+// EXERCISE A
+/*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
     What happens if we change from a LEFT join to an INNER join?
 
@@ -15,10 +28,10 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
-}
 
-function findById(scheme_id) { // EXERCISE B
-  /*
+// function findById(scheme_id) {
+// EXERCISE B
+/*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
       SELECT
@@ -83,10 +96,10 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
-}
 
-function findSteps(scheme_id) { // EXERCISE C
-  /*
+// function findSteps(scheme_id) {
+// EXERCISE C
+/*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
     should be empty if there are no steps for the scheme:
@@ -106,26 +119,17 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
-}
 
-function add(scheme) { // EXERCISE D
-  /*
+// function add(scheme) {
+// EXERCISE D
+/*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
-}
 
-function addStep(scheme_id, step) { // EXERCISE E
-  /*
+// function addStep(scheme_id, step) {
+// EXERCISE E
+/*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
-}
-
-module.exports = {
-  find,
-  findById,
-  findSteps,
-  add,
-  addStep,
-}
